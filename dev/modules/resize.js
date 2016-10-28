1a5b41b8bd57d997f239e87acdfecf3043e16e09
Luis Soares <luis.soares@comon.pt>
2016-10-28 14:36:07 +0100

    New methods for resize module

--- dev/modules/resize.js
+++ dev/modules/resize.js
@@ -54,21 +54,45 @@ module.exports = function (cylinder, _module) {
 		return width >= rule.width_min && (_.isNumber(rule.width_max) ? width <= rule.width_max : true);
 	}
 
+	// just a function that serves as a shortcut to execute a rule
+	// and, at the same time, apply classes to body or whatever other stuff that's needed
+	function evaluateRule (width, height, rule, name) {
+		var position = currentRules.indexOf(name);
+		var predicate = (_.isFunction(rule.callback) ? rule.callback : defaultRuleCallback)(width, height, rule);
+
+		// add or remove to current rules array
+		if (predicate && position === -1) currentRules.push(name);
+		else if (!predicate && position !== -1) currentRules.splice(position, 1);
+
+		// 
+		cylinder.dom.$body.toggleClass(name, predicate);
+		return predicate;
+	}
+
+	var currentRules = [];
 	var rules = {
 		// THIS WILL HOLD THE DEFAULT RULES!
 		// These rules are based on sizes from Bootstrap v4.0.0-alpha.2
-		'layout-xs': new CylinderResizeRule({ width_min: 0, width_max: 543 }),
-		'layout-sm': new CylinderResizeRule({ width_min: 544, width_max: 767 }),
-		'layout-md': new CylinderResizeRule({ width_min: 768, width_max: 991 }),
-		'layout-lg': new CylinderResizeRule({ width_min: 992, width_max: 1199 }),
-		'layout-xl': new CylinderResizeRule({ width_min: 1200 })
+		'bs4-xs': new CylinderResizeRule({ width_min: 0, width_max: 543 }),
+		'bs4-sm': new CylinderResizeRule({ width_min: 544, width_max: 767 }),
+		'bs4-md': new CylinderResizeRule({ width_min: 768, width_max: 991 }),
+		'bs4-lg': new CylinderResizeRule({ width_min: 992, width_max: 1199 }),
+		'bs4-xl': new CylinderResizeRule({ width_min: 1200 })
+	};
+
+	/**
+	 * Returns a collection of names and their CylinderResizeRules.
+	 * @return {Array.<CylinderResizeRule>} The collection of rules.
+	 */
+	module.rules = function () {
+		return rules;
 	};
 
 	/**
 	 * Adds a rule to the module.
 	 *
-	 * @param    {String}             name - The name of the rule to add.
-	 * @param    {CylinderResizeRule} rule - The rule object to add.
+	 * @param {String}             name - The name of the rule to add.
+	 * @param {CylinderResizeRule} rule - The rule object to add.
 	 */
 	module.addRule = function (name, rule) {
 		// if the passed rule is not an instance of CylinderResizeRule,
@@ -83,26 +107,45 @@ module.exports = function (cylinder, _module) {
 		if (module.width !== null && module.height !== null) {
 			// if the module already triggered a resize event,
 			// then, from now on, we'll always evaluate new rules as soon as they're added.
-			cylinder.dom.$body.toggleClass(name, (_.isFunction(rule.callback) ? rule.callback : defaultRuleCallback)(module.width, module.height, rule));
+			evaluateRule(module.width, module.height, rule, name);
 		}
-	}
+	};
+
+	/**
+	 * Returns a specific CylinderResizeRules instance from the module.
+	 *
+	 * @param   {String} name - The name of the rule to return.
+	 * @returns {CylinderResizeRule} Rule instance.
+	 */
+	module.getRule = function (name) {
+		return rules[name] || null;
+	};
+
+	/**
+	 * Returns a list of currently applied rules.
+	 * If `true` is passed to the method, it will return an object
+	 * matching the names against the rules' instances themselves.
+	 *
+	 * @param   {Boolean} objects - If true, the method will return the rules themselves.
+	 * @returns {Array|Object} List (or dictionary) of currently applied rules.
+	 */
+	module.getCurrentRules = function (objects) {
+		if (objects !== true) {
+			return currentRules;
+		}
+
+		var ruleInstances = _.map(currentRules, function (name) { return rules[name]; });
+		return _.object(currentRules, ruleInstances);
+	};
 
 	/**
 	 * Removes a rule from the module.
 	 *
-	 * @param    {String} name - The name of the rule to remove.
+	 * @param {String} name - The name of the rule to remove.
 	 */
 	module.removeRule = function (name) {
 		delete rules[name];
-	}
-
-	/**
-	 * Returns a collection of names and their CylinderResizeRules.
-	 * @return   {Array.<CylinderResizeRule>} The collection of rules.
-	 */
-	module.rules = function () {
-		return rules;
-	}
+	};
 
 	// this is the method that will handle resizing!
 	// will calc values, call styles according to rules,
@@ -119,7 +162,7 @@ module.exports = function (cylinder, _module) {
 		// we'll use the 'callback' property to evaluate that.
 		_.each(rules, function (rule, name) {
 			if (!(rule instanceof CylinderResizeRule)) return;
-			cylinder.dom.$body.toggleClass(name, (_.isFunction(rule.callback) ? rule.callback : defaultRuleCallback)(module.width, module.height, rule));
+			evaluateRule(module.width, module.height, rule, name);
 		});
 
 		// call the event!
