5ff6b3f3336aed7c3ed1a2981d7d5c0d39f4db0b
Luis Soares <luis.soares@comon.pt>
2017-01-25 12:59:44 +0000

    Removing uglify:pkgd in favor of concat:pkgd

---
+++ grunt/concat.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+	options: {
+		nonull: true,
+		sourceMap: true,
+		separator: ';'
+	},
+
+	pkgd: {
+		files: {
+			'<%= project.dist %>/cylinder.pkgd.js': [
+				'<%= project.bower %>/async/dist/async.min.js',
+				'<%= project.bower %>/jquery/dist/jquery.min.js',
+				'<%= project.bower %>/underscore/underscore-min.js',
+				'<%= project.bower %>/underscore.string/dist/underscore.string.min.js',
+				'<%= project.bower %>/backbone/backbone-min.js',
+				'<%= project.bower %>/mustache.js/mustache.min.js',
+				'<%= project.dist %>/cylinder.min.js'
+			]
+		}
+	}
+
+};
